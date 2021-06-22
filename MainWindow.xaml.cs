a50317818e6183f1aa6ad694c5899c534d26b686
Jean Barros <ac.jbarros@gmail.com>
2021-06-22 10:36:52 -0300

    Project finished and working properly.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace Space_Shooter
 {
     /// <summary>
@@ -20,9 +22,218 @@ namespace Space_Shooter
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool moveLeft, moveRight;
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
+        Random rand = new Random();
+
+        int enemySpriteCounter = 0;
+        int enemyCounter = 100;
+        int playerSpeed = 10;
+        int limit = 50;
+        int score = 0;
+        int damage = 0;
+        int enemySpeed = 10;
+
+        Rect playerHitBox;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+            Stage.Focus();
+
+            ImageBrush bg = new ImageBrush();
+
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewboxUnits = BrushMappingMode.RelativeToBoundingBox;
+            Stage.Background = bg;
+
+            ImageBrush playerTexture = new ImageBrush();
+            playerTexture.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/player.png"));
+            player.Fill = playerTexture;
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            playerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+
+            enemyCounter -= 1;
+
+            scoreTxt.Content = "Score: " + score;
+            damageTxt.Content = "Damage: " + damage;
+
+            if (enemyCounter < 0)
+            {
+                MakeEnemies();
+                enemyCounter = limit;
+            }
+
+            if (moveLeft && Canvas.GetLeft(player) > 0)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed);
+            }
+            if (moveRight && Canvas.GetLeft(player) + 90 < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
+            }
+
+            foreach (var item in Stage.Children.OfType<Rectangle>())
+            {
+                if (item is Rectangle && (string)item.Tag == "bullet")
+                {
+                    Canvas.SetTop(item, Canvas.GetTop(item) - 20);
+
+                    Rect bulletHitBox = new Rect(Canvas.GetLeft(item), Canvas.GetTop(item), item.Width, item.Height);
+
+                    if (Canvas.GetTop(item) < 10)
+                    {
+                        itemRemover.Add(item);
+                    }
+
+                    foreach (var enemy in Stage.Children.OfType<Rectangle>())
+                    {
+                        if (enemy is Rectangle && (string)enemy.Tag == "enemy")
+                        {
+                            Rect enemyHit = new Rect(Canvas.GetLeft(enemy), Canvas.GetTop(enemy), enemy.Width, enemy.Height);
+
+                            if (bulletHitBox.IntersectsWith(enemyHit))
+                            {
+                                itemRemover.Add(item);
+                                itemRemover.Add(enemy);
+                                score++;
+                            }
+                        }
+                    }
+                }
+
+                if (item is Rectangle && (string)item.Tag == "enemy")
+                {
+                    Canvas.SetTop(item, Canvas.GetTop(item) + enemySpeed);
+
+                    if (Canvas.GetTop(item) > 750)
+                    {
+                        itemRemover.Add(item);
+                        damage += 10;
+                    }
+
+                    Rect enemyHitBox = new Rect(Canvas.GetLeft(item), Canvas.GetTop(item), item.Width, item.Height);
+
+                    if (playerHitBox.IntersectsWith(enemyHitBox))
+                    {
+                        itemRemover.Add(item);
+                        damage += 5;
+                    }
+                }
+            }
+
+            foreach (Rectangle destroyedItem in itemRemover)
+            {
+                Stage.Children.Remove(destroyedItem);
+            }
+
+            if (score > 5)
+            {
+                limit = 20;
+                enemySpeed = 15;
+            }
+
+            if (damage > 99)
+            {
+                gameTimer.Stop();
+                damageTxt.Content = "Damage: 100";
+                damageTxt.Foreground = Brushes.Red;
+                MessageBox.Show("Captain, You have destroyed " + score + "Alien Ships" + Environment.NewLine + "Press Ok to play again", "Ha ha ha!");
+
+                System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = true;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = false;
+            }
+
+            if (e.Key == Key.Space)
+            {
+                Rectangle newBullet = new Rectangle
+                {
+                    Tag = "bullet",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red
+                };
+
+                Canvas.SetLeft(newBullet, Canvas.GetLeft(player) + player.Width / 2);
+                Canvas.SetTop(newBullet, Canvas.GetTop(player) - newBullet.Height);
+
+                Stage.Children.Add(newBullet);
+            }
+        }
+
+        private void MakeEnemies()
+        {
+            ImageBrush enemySprite = new ImageBrush();
+
+            enemySpriteCounter = rand.Next(1, 5);
+
+            switch (enemySpriteCounter)
+            {
+                case 1:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
+                    break;
+                case 2:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
+                    break;
+                case 3:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/3.png"));
+                    break;
+                case 4:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/4.png"));
+                    break;
+                case 5:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/5.png"));
+                    break;
+            }
+
+            Rectangle newEnemy = new Rectangle
+            {
+                Tag = "enemy",
+                Height = 50,
+                Width = 56,
+                Fill = enemySprite
+            };
+
+            Canvas.SetTop(newEnemy, -100);
+            Canvas.SetLeft(newEnemy, rand.Next(30, 430));
+            Stage.Children.Add(newEnemy);
         }
     }
 }
